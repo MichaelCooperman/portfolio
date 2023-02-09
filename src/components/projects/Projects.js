1d98277c5bc84cccc51135796f0883859075c92b
Michael <Mich4elcoop321@gmail.com>
2023-02-09 14:06:54 -0500

    project 3 added

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -4,7 +4,6 @@ import ProjectCards from "./ProjectCards";
 import test1 from "../../images/test1.jpg";
 import test2 from "../../images/test2.jpg";
 import test3 from "../../images/test3.jpg";
-// import { DiSass, DiReact } from "react-icons/di";
 
 const Projects = () => {
   return (
@@ -16,7 +15,7 @@ const Projects = () => {
         <ProjectCards
           site="https://daydreamcookies.netlify.app/"
           img={test1}
-          alt="Project 1 Image"
+          alt="Daydream Cookies Image"
           title="Daydream Cookies"
           description="Showcases fully functional shopping experience. Including a working cart modal made with React and Sass."
           git="https://github.com/MichaelCooperman/Cookie-Store"
@@ -24,18 +23,18 @@ const Projects = () => {
         <ProjectCards
           site="https://airbnb-clone-mc.netlify.app/"
           img={test2}
-          alt="Project 2 Image"
+          alt="Airbnb Clone Image"
           title="Airbnb Clone"
           description="Uses an API and database to present rental homes. Category feature for different types of homes, made using React and Sass."
           git="https://github.com/MichaelCooperman/Airbnb-Clone"
         />
         <ProjectCards
-          site="https://www.google.com/"
+          site="https://front-end-quiz-app.netlify.app/"
           img={test3}
-          alt="Project 3 Image"
-          title="Project 3"
-          description="Project 3 is under construction and will be up soon!"
-          git="https://www.google.com/"
+          alt="Front-End Quiz Image"
+          title="Front-End Quiz"
+          description="Fun study guide for anyone learning Front-End Web Development, used use-context to access props globally. Made with React, React Router and Sass."
+          git="https://github.com/MichaelCooperman/Front-End-Quiz"
         />
       </div>
     </section>
